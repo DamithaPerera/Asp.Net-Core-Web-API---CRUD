cb79a4b8300365f1eb4a1709f9979df7bc035d5b
Damitha Perera <damithap@99x.io>
2023-11-23 17:29:48 +0530

    libs add and model crated

---
+++ WebApplication1/WebApplication1/models/Brand.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.models
+{
+    public class Brand
+    {
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
