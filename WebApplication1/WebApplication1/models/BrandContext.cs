b740b9ede1ceb8de5aaf2b15363aaa7c97543ca4
Damitha Perera <damithap@99x.io>
2023-11-23 17:59:30 +0530

    DB connection

--- WebApplication1/WebApplication1/models/BrandContext.cs
+++ WebApplication1/WebApplication1/models/BrandContext.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApplication1.models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.models
 {
-    public class BrandContext
+    public class BrandContext: DbContext
     {
+        // constructure
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options) { 
+
+        }
+
+        public DbSet<Brand> Brands { get; set; }
     }
 }
